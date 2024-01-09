be0c5508978ff4aa283ee400d9702b93493e9e28
Eric Springer <ericspringer@outlook.com>
2024-01-08 23:24:26 -0300

    conserto de bugs de responsividade, mudança de cores

--- mobile.js
+++ mobile.js
@@ -1,34 +1,38 @@
+const bodyHtml = document.querySelector('body');
+const linksNav = document.querySelectorAll('.link')
+
 class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks) {
-      this.mobileMenu = document.querySelector(mobileMenu);
-      this.navList = document.querySelector(navList);
-      this.navLinks = document.querySelectorAll(navLinks);
-      this.activeClass = "active";
-  
-      this.handleClick = this.handleClick.bind(this);
-    }
-  
-    handleClick() {
-      this.navList.classList.toggle(this.activeClass);
-      this.mobileMenu.classList.toggle(this.activeClass);
-      this.animateLinks();
-    }
-  
-    addClickEvent() {
-      this.mobileMenu.addEventListener("click", this.handleClick);
-    }
-  
-    init() {
-      if (this.mobileMenu) {
-        this.addClickEvent();
-      }
-      return this;
-    }
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+
+    this.mobileMenu.addEventListener("click", () => this.handleClick());
+    this.addLinkClickEvent();
   }
-  
-  const mobileNavbar = new MobileNavbar(
-    ".mobile-menu",
-    ".nav-list",
-    ".nav-list li",
-  );
-  mobileNavbar.init();
+
+  handleClick() {
+    [this.navList, this.mobileMenu, bodyHtml].forEach(elemento => elemento.classList.toggle(this.activeClass));
+    // this.animateLinks(); // Se necessário, você pode chamar a função de animação aqui
+  }
+
+  addLinkClickEvent() {
+    this.navLinks.forEach(link => {
+      link.addEventListener("click", () => {
+        this.removeActiveFromAll();
+      });
+    });
+  }
+
+  removeActiveFromAll() {
+    [this.navList, this.mobileMenu, bodyHtml].forEach(elemento => elemento.classList.remove(this.activeClass));
+    // Lógica adicional aqui, se necessário
+  }
+}
+
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li",
+);
